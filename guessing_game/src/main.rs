c1979b62f3f9cae93f30e96b2b469ba4990958bc
sky <skyblue3350@gmail.com>
2018-12-07 18:19:45 +0900

    ループするように変更

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,23 +10,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // 可変の場合はmutを付ける
-    let mut guess = String::new();
+        // 可変の場合はmutを付ける
+        let mut guess = String::new();
 
-    // ioライブラリから標準入力の受け取り
-    // useしない場合はフルパスを書く
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        // ioライブラリから標準入力の受け取り
+        // useしない場合はフルパスを書く
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    // 比較用に型を揃える
-    let guess: u32 = guess.trim().parse().expect("please type number");
+        // 比較用に型を揃える
+        let guess: u32 = guess.trim().parse().expect("please type number");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal   => println!("You win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal   => println!("You win"),
+        }
     }
 }

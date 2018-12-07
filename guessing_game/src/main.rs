74d2974ffcc9db7283cf892fb1f98dc1ac6df5eb
sky <skyblue3350@gmail.com>
2018-12-07 18:17:51 +0900

    雛形できた

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -19,4 +20,13 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    // 比較用に型を揃える
+    let guess: u32 = guess.trim().parse().expect("please type number");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal   => println!("You win"),
+    }
 }

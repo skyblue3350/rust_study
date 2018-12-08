6a37f506e7821ac69022d5b378f109498e59d666
sky <skyblue3350@gmail.com>
2018-12-08 14:43:10 +0900

    sleepするように

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 // 構造体
 struct Philosopher {
     name: String,
@@ -13,7 +16,12 @@ impl Philosopher {
     }
 
     fn eat(&self) {
+        println!("{} is eating.", self.name);
+
+        thread::sleep(Duration::from_millis(1000));
+
         println!("{} is done eating", self.name);
+
     }
 }
 
